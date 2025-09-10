a06411bed5673e209629c218277259d524180b20
Hamisi_Juma <hamisig123@gmail.com>
2025-09-10 15:56:37 +0300

    Created basic_operations.py

---
+++ basic_operations.py
@@ -0,0 +1,14 @@
+
+# number1 = int(input("Enter 1st number: "))
+# number2 = int(input("Enter 2nd number: "))
+
+number1 = 10
+number2 = 5
+
+sum = number1 + number2
+diff = number1 - number2
+product = number1 * number2
+
+print( f"Addition of {number1} and {number2} is {sum}")
+print( f"Subtraction of {number1} and {number2} is {diff}")
+print( f"Multiplication of {number1} and {number2} is {product}")
