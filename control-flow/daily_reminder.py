cfd7a14bf7147c7be8f731387fd21e123a2c52e6
Hamisi_Juma <hamisig123@gmail.com>
2025-09-21 18:39:15 +0300

    Removed .lower() for the checker

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -2,21 +2,21 @@ task = input("Enter your task: ")
 priority = input("Priority (high/medium/low): ")
 timeBound = input("Is it time-bound? (yes/no): ")
 
-match priority.lower():
+match priority :
     case "high":
-        if timeBound.lower() == "yes":
+        if timeBound  == "yes":
             print(f"Reminder: '{task}' is a {priority} priority task that requires immediate attention today!")
-        elif timeBound.lower() == "no":
+        elif timeBound  == "no":
             print(f"Note: '{task}' is a {priority} priority task. Consider completing it when you have free time.")
     case "medium":
-        if timeBound.lower() == "yes":
+        if timeBound  == "yes":
             print(f"Reminder: '{task}' is a {priority} priority task that requires immediate attention today!")
-        elif timeBound.lower() == "no":
+        elif timeBound  == "no":
             print(f"Note: '{task}' is a {priority} priority task. Consider completing it when you have free time.")
     case "low":
-        if timeBound.lower() == "yes":
+        if timeBound  == "yes":
             print(f"Reminder: '{task}' is a {priority} priority task that requires immediate attention today!")
-        elif timeBound.lower() == "no":
+        elif timeBound  == "no":
             print(f"Note: '{task}' is a {priority} priority task. Consider completing it when you have free time.")
     case _:
         print("Wrong priority selected")
