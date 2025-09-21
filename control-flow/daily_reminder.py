f9b733a1b69c0c0c659d101f41a4f6313dc54bbd
Hamisi_Juma <hamisig123@gmail.com>
2025-09-21 18:42:08 +0300

    Making changes for the checker

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -6,17 +6,17 @@ match priority :
     case "high":
         if timeBound  == "yes":
             print(f"Reminder: '{task}' is a {priority} priority task that requires immediate attention today!")
-        elif timeBound  == "no":
+        else:
             print(f"Note: '{task}' is a {priority} priority task. Consider completing it when you have free time.")
     case "medium":
         if timeBound  == "yes":
             print(f"Reminder: '{task}' is a {priority} priority task that requires immediate attention today!")
-        elif timeBound  == "no":
+        else:
             print(f"Note: '{task}' is a {priority} priority task. Consider completing it when you have free time.")
     case "low":
         if timeBound  == "yes":
             print(f"Reminder: '{task}' is a {priority} priority task that requires immediate attention today!")
-        elif timeBound  == "no":
+        else:
             print(f"Note: '{task}' is a {priority} priority task. Consider completing it when you have free time.")
     case _:
         print("Wrong priority selected")
