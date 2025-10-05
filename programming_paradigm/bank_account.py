74c51546b2e29fa506e5376adf588bf7040e383d
Hamisi_Juma <hamisig123@gmail.com>
2025-10-05 16:44:47 +0300

    Made a correction on the withdraw function

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -4,7 +4,8 @@ class BankAccount:
     def deposit(self, amount):
         self.account_balance += amount
     def withdraw(self, amount):
-        if self.account_balance <= amount:
+        if self.account_balance >= amount:
+            self.account_balance -= amount
             return True
         else:
             return False
